48dd62f5e3e6a92c7afac48a22a2200e7dafcbaf
Eyeroniq <133900191+Eyeroniq@users.noreply.github.com>
2025-05-04 20:21:14 +0530

    Delete 2 directory

--- 2/ConcatInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-// This interface defines the remote method
-public interface ConcatInterface extends Remote {
-    String concatenate(String s1, String s2) throws RemoteException;
-}
